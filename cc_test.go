f95926264b6553d8a918f8e82ac15a053f57f10c
cquestor <3206293873@qq.com>
2022-12-09 21:21:36 +0800

    jwt

--- cc_test.go
+++ cc_test.go
@@ -1,16 +1,32 @@
 package cc_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cquestor/cc"
 )
 
 func TestMain(m *testing.M) {
-	c := cc.Default()
+	// c := cc.Default()
 
-	c.GET("/", func(c *cc.Context) {
-	})
+	// c.GET("/", func(c *cc.Context) {
+	// })
 
-	c.Run()
+	// c.Run()
+	type user struct {
+		Name string
+	}
+	result, err := cc.JWTToken(user{Name: "nihao"}, "cquestor")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+	fmt.Println(cc.JWTCheck(result, "cquestor"))
+	var test user
+	err = cc.JWTParse(result, &test)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(test.Name)
 }
--- utils.go
+++ utils.go
@@ -1,8 +1,20 @@
 package cc
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strings"
 	"unicode"
 )
+
+type tokenHeader struct {
+	Alg string `json:"alg"`
+	Typ string `json:"typ"`
+}
 
 func camel2Case(s string) string {
 	var output []rune
@@ -17,4 +29,49 @@ func camel2Case(s string) string {
 		}
 	}
 	return string(output)
+}
+
+func JWTToken(payload any, secret string) (string, error) {
+	header := tokenHeader{Alg: "HS256", Typ: "JWT"}
+	json_header, err := json.Marshal(header)
+	if err != nil {
+		return "", err
+	}
+	base64_header := base64.StdEncoding.EncodeToString(json_header)
+	json_payload, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+	base64_payload := base64.StdEncoding.EncodeToString(json_payload)
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(fmt.Sprintf("%s.%s", base64_header, base64_payload)))
+	signature := hex.EncodeToString(h.Sum(nil))
+	return fmt.Sprintf("%s.%s.%s", base64_header, base64_payload, signature), nil
+}
+
+func JWTCheck(token string, secret string) (bool, error) {
+	infos := strings.Split(token, ".")
+	sig, err := hex.DecodeString(infos[2])
+	if err != nil {
+		return false, err
+	}
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(fmt.Sprintf("%s.%s", infos[0], infos[1])))
+	if !hmac.Equal(sig, h.Sum(nil)) {
+		return false, nil
+	}
+	return true, nil
+}
+
+func JWTParse(token string, result any) error {
+	infos := strings.Split(token, ".")
+	payload, err := base64.StdEncoding.DecodeString(infos[1])
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(payload, result)
+	if err != nil {
+		return err
+	}
+	return nil
 }
